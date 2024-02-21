fd28667f153c20ba4ac54497649fe520c76d83bd
Thomas Flassbeck <t.flassbeck@simfin.com>
2024-02-21 10:46:20 +0100

    interface change

--- parsee/storage/vector_stores/interfaces.py
+++ parsee/storage/vector_stores/interfaces.py
@@ -5,11 +5,5 @@ from parsee.extraction.extractor_elements import StandardDocumentFormat, Extract
 
 class VectorStore:
 
-    def delete_all_from_document(self, source_identifier: str):
-        raise NotImplemented
-
-    def upload_document(self, document: StandardDocumentFormat):
-        raise NotImplemented
-
     def find_closest_elements(self, document: StandardDocumentFormat, search_element_title: str, keywords: str, tables_only: bool = True) -> List[ExtractedEl]:
         raise NotImplemented
