952d5ff30392ebaf5eea50db0a9f6dd4a0d1ee7f
Thomas Flassbeck <t.flassbeck@simfin.com>
2024-03-07 13:03:43 +0100

    pdf reader refactoring

--- parsee/converters/pdf_extraction.py
+++ parsee/converters/pdf_extraction.py
@@ -8,7 +8,7 @@ from pdf_reader import get_elements_from_pdf
 from parsee.converters.interfaces import RawToJsonConverter
 from parsee.utils.enums import DocumentType, ElementType
 from pdf_reader.custom_dataclasses import RelativeAreaPrediction
-from pdf_reader.custom_dataclasses import ExtractedPage
+from pdf_reader.custom_dataclasses import ExtractedPage, ExtractedTable, ExtractedPdfElement, ExtractedFigure, LineItem
 from parsee.utils.helper import is_year_cell, is_number_cell
 
 
@@ -26,20 +26,21 @@ class PdfConverter(RawToJsonConverter):
         elements: List[ExtractedEl] = []
         for page_num, page in enumerate(pages):
             for idx, el in enumerate(page.paragraphs):
-                dict_el = el.dict_el
-                element = None
-                if dict_el["type"] == "et":
+                if isinstance(el, ExtractedTable):
                     element = self._make_structured_table(
                         ExtractedSource(DocumentType.PDF, {"x0": el.x0, "x1": el.x1, "y0": el.y0, "y1": el.y1}, None,
-                                        len(elements), {"page_idx": page_num, "page_size": [page.size.x0, page.size.y0, page.size.x1, page.size.y1]}), dict_el)
-                elif dict_el["type"] == "em":
+                                        len(elements), {"page_idx": page_num, "page_size": [page.size.x0, page.size.y0, page.size.x1, page.size.y1]}), el)
+                elif isinstance(el, ExtractedFigure):
+                    element = ExtractedEl(ElementType.FIGURE, ExtractedSource(DocumentType.PDF, {"x0": el.x0, "x1": el.x1, "y0": el.y0, "y1": el.y1}, None, len(elements),
+                                                                              {"page_idx": page_num, "page_size": [page.size.x0, page.size.y0, page.size.x1, page.size.y1]}), None)
+                elif isinstance(el, ExtractedPdfElement):
                     element = ExtractedEl(ElementType.TEXT, ExtractedSource(DocumentType.PDF,
                                                                             {"x0": el.x0, "x1": el.x1, "y0": el.y0,
                                                                              "y1": el.y1}, None, len(elements),
                                                                             {"page_idx": page_num,
                                                                              "page_size": [page.size.x0, page.size.y0, page.size.x1, page.size.y1]}), el.get_text())
-                elif dict_el["type"] == "ef":
-                    element = ExtractedEl(ElementType.FIGURE, ExtractedSource(DocumentType.PDF, {"x0": el.x0, "x1": el.x1, "y0": el.y0, "y1": el.y1}, None, len(elements), {"page_idx": page_num, "page_size": [page.size.x0, page.size.y0, page.size.x1, page.size.y1]}), None)
+                else:
+                    raise Exception("element not recognized")
                 elements.append(element)
         return elements
 
@@ -49,16 +50,16 @@ class PdfConverter(RawToJsonConverter):
 
         return self.pages_to_extracted_el(pages), PRICING_PDF_CONVERSION
 
-    def _make_structured_table(self, source: ExtractedSource, extracted_table_dict) -> StructuredTable:
+    def _make_structured_table(self, source: ExtractedSource, table: ExtractedTable) -> StructuredTable:
 
         rows_structured: List[StructuredRow] = []
 
         # determine header end
         header_end_idx = 0
-        for k, row in enumerate(extracted_table_dict['i']):
-            found_valid = False if row['c'] == '' else True
-            for val in row['v']:
-                if not is_number_cell(val['v']) or is_year_cell(val['v']):
+        for k, line_item in enumerate(table.items):
+            found_valid = False if line_item.caption == '' else True
+            for v in line_item.values:
+                if not is_number_cell(v.val) or is_year_cell(v.val):
                     found_valid = False
                     break
             if found_valid:
@@ -66,25 +67,17 @@ class PdfConverter(RawToJsonConverter):
                 break
 
         # add value rows
-        for k, row in enumerate(extracted_table_dict['i']):
+        for k, row in enumerate(table.items):
             rows_structured.append(self._make_structured_row("body" if k >= header_end_idx else "header", row))
 
         return StructuredTable(source, rows_structured)
 
-    def _make_structured_row(self, row_type: str, row) -> StructuredRow:
+    def _make_structured_row(self, row_type: str, row: LineItem) -> StructuredRow:
         row_values: List[StructuredTableCell] = []
-        li_caption_cell = StructuredTableCell(row['c'])
+        li_caption_cell = StructuredTableCell(row.caption)
         row_values.append(li_caption_cell)
-        for val in row['v']:
-            val_obj = self._make_structured_cell_from_dict(val)
+        for v in row.values:
+            val_obj = StructuredTableCell(v.val)
             if val_obj.valid:
                 row_values.append(val_obj)
         return StructuredRow(row_type, row_values)
-
-    def _make_structured_cell_from_dict(self, val_dict) -> StructuredTableCell:
-
-        colspan = 1
-        # value item
-        val = str(val_dict['v']) if val_dict['v'] is not None else ""
-
-        return StructuredTableCell(val, colspan)
