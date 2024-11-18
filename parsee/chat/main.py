6609cfd5f7786b2d73028a7c1a57a09541491ba5
Parsee.ai <t.flassbeck@simfin.com>
2024-11-18 13:51:00 +0100

    merging answer pieces

--- parsee/chat/main.py
+++ parsee/chat/main.py
@@ -5,6 +5,7 @@ from parsee.storage.interfaces import DocumentManager
 from parsee.extraction.models.model_dataclasses import MlModelSpecification
 from parsee.extraction.models.model_loader import get_llm_base_model
 from parsee.extraction.models.llm_models.prompts import Prompt
+from parsee.utils.helper import merge_answer_pieces
 
 
 def run_chat(message: Message, message_history: List[Message], document_manager: DocumentManager, receivers: List[MlModelSpecification], most_recent_references_only: bool, show_chunk_index: bool = False, single_page_processing_max_images_trigger: Optional[int] = None) -> List[Message]:
@@ -33,18 +34,19 @@ def run_chat(message: Message, message_history: List[Message], document_manager:
             if single_page_processing_max_images_trigger is not None and len(data) >= single_page_processing_max_images_trigger:
                 process_pages_individually = True
         if process_pages_individually:
-            answer = ""
+            answers = []
             cost = Decimal(0)
             for k, img in enumerate(data):
                 if k > 0:
-                    additional_info = f"We are showing you the images contained in the document one by one. The current image is number {k + 1} out of a total of {len(data)}.\n Your last message ended with the following (make sure to continue the output if the requested format is JSON or similar; last 300 characters are shown):\n" \
-                                      f"{answer[-300:]}"
+                    additional_info = f"We are showing you the images contained in the document one by one. The current image is number {k + 1} out of a total of {len(data)}.\n Your last answer ended with the following (make sure that your new answer is valid JSON or similar, as requested; last 500 characters are shown):\n" \
+                                      f"{answers[-1][-500:]}"
                 else:
                     additional_info = f"We are showing you the images contained in the document one by one. The current image is number {k + 1} out of a total of {len(data)}."
                 prompt = Prompt(None, f"{message}", additional_info=additional_info, available_data=[img], history=[str(m) for m in message_history])
                 current_answer, current_cost = model.make_prompt_request(prompt)
-                answer += current_answer
+                answers.append(current_answer)
                 cost += current_cost
+            answer = merge_answer_pieces(answers)
         else:
             prompt = Prompt(None, f"{message}", available_data=data, history=[str(m) for m in message_history])
             answer, cost = model.make_prompt_request(prompt)
